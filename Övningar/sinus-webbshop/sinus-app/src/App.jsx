3ef6839a27df646347ffe23fc0d2e1700fa23a72
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-04-04 23:26:09 +0200

    sinus uppladda

---
+++ Övningar/sinus-webbshop/sinus-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import products from './assets/data.js';
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <h1>Sinus Webbshop</h1>
+      
+    </div>
+  )
+}
+
+export default App
